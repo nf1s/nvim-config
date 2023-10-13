4886d1a677e912c10bdef1f2f32b54f48b796080
Ahmed Nafies <ahmed.nafies@gmail.com>
2023-10-13 12:05:36 +0200

    feat: adds fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
   tag = "0.1.3",
   dependencies = {
     "nvim-lua/plenary.nvim",
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   }
   ,
   config = function()
@@ -15,8 +16,17 @@ return {
 		prompt_position = "top",
 			}
 		},
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+    },
+  }
 	}
 
+  require('telescope').load_extension('fzf')
 	local builtin = require("telescope.builtin")
 	vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 	vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
