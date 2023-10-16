4050af115ddf569fdcbc20c50852b8c04cb1c35b
Ahmed Nafies <ahmed.nafies@gmail.com>
2023-10-16 14:32:39 +0200

    refactor: renames

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,7 +9,7 @@ return {
   config = function()
 	require('nvim-treesitter.configs').setup {
 	  -- A list of parser names, or "all"
-	  ensure_installed = { "go", "lua", "python", "rust" },
+	  ensure_installed = { "go", "lua", "python", "rust", "yaml"  },
 
 	  -- Install parsers synchronously (only applied to `ensure_installed`)
 	  sync_install = false,
