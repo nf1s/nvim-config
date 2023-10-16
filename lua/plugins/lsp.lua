25383b3bdfe85f77eb0b805cc8fe5605e3c5e52f
Ahmed Nafies <ahmed.nafies@gmail.com>
2023-10-16 10:50:14 +0200

    fix: adds copilot and minimizes lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,9 +6,6 @@ return {
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-vsnip",
-    "hrsh7th/vim-vsnip",
-    "hrsh7th/cmp-nvim-lsp-signature-help",
   },
 
   config = function()
@@ -90,11 +87,6 @@ return {
     end
 
     cmp.setup({
-      snippet = {
-        expand = function(args)
-          vim.fn["vsnip#anonymous"](args.body)
-        end,
-      },
       window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
@@ -103,31 +95,11 @@ return {
         ["<C-Up>"] = cmp.mapping.scroll_docs(-4),
         ["<C-Down>"] = cmp.mapping.scroll_docs(4),
         ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif vim.fn["vsnip#available"](1) == 1 then
-            feedkey("<Plug>(vsnip-expand-or-jump)", "")
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function()
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-            feedkey("<Plug>(vsnip-jump-prev)", "")
-          end
-        end, { "i", "s" }),
       }),
 
       sources = cmp.config.sources({
         { name = "path" },
         { name = "nvim_lsp" },
-        { name = "vsnip" },
-        { name = "nvim_lsp_signature_help" },
         { name = "spell" },
         { name = "buffer" },
       }),
