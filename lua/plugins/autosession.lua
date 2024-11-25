c4266d7e0b5c0374d6b81f112c77a64b25a13aa7
Ahmed Nafies <ahmed.nafies@gmail.com>
2024-11-25 13:43:34 +0100

    feat: fixes gen, mason, session and wildfire

--- lua/plugins/autosession.lua
+++ lua/plugins/autosession.lua
@@ -1,6 +1,7 @@
 return {
   {
     'rmagatti/auto-session',
+    lazy = false,
     config = function()
       require('auto-session').setup({
         auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
